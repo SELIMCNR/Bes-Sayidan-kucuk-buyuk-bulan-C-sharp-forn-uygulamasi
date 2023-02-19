71d3c5a9d2161b78be6cd36cf2eba8cabb7cf0c0
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2023-02-19 20:20:35 +0300

    C-Sharp-Form

---
+++ GirilenBesSayidanBuyukKucukBulma/GirilenOnSayidanBuyukKucukBulma/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace GirilenOnSayidanBuyukKucukBulma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtSayı1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtSayi2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtSayi3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtSayi5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtSayi4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Txt_kucuk = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Txt_buyuk = new System.Windows.Forms.TextBox();
+            this.Btn_Bul = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 40);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sayı 1";
+            // 
+            // TxtSayı1
+            // 
+            this.TxtSayı1.Location = new System.Drawing.Point(78, 40);
+            this.TxtSayı1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.TxtSayı1.Name = "TxtSayı1";
+            this.TxtSayı1.Size = new System.Drawing.Size(121, 29);
+            this.TxtSayı1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(258, 40);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Sayı 2";
+            // 
+            // TxtSayi2
+            // 
+            this.TxtSayi2.Location = new System.Drawing.Point(330, 40);
+            this.TxtSayi2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.TxtSayi2.Name = "TxtSayi2";
+            this.TxtSayi2.Size = new System.Drawing.Size(121, 29);
+            this.TxtSayi2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(488, 40);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 24);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Sayı 3";
+            // 
+            // TxtSayi3
+            // 
+            this.TxtSayi3.Location = new System.Drawing.Point(560, 40);
+            this.TxtSayi3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.TxtSayi3.Name = "TxtSayi3";
+            this.TxtSayi3.Size = new System.Drawing.Size(121, 29);
+            this.TxtSayi3.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(385, 97);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 24);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Sayı 5";
+            // 
+            // TxtSayi5
+            // 
+            this.TxtSayi5.Location = new System.Drawing.Point(457, 97);
+            this.TxtSayi5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.TxtSayi5.Name = "TxtSayi5";
+            this.TxtSayi5.Size = new System.Drawing.Size(121, 29);
+            this.TxtSayi5.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(125, 97);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 24);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Sayı 4";
+            // 
+            // TxtSayi4
+            // 
+            this.TxtSayi4.Location = new System.Drawing.Point(197, 97);
+            this.TxtSayi4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.TxtSayi4.Name = "TxtSayi4";
+            this.TxtSayi4.Size = new System.Drawing.Size(121, 29);
+            this.TxtSayi4.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(254, 236);
+            this.label6.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 24);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "En Küçük";
+            // 
+            // Txt_kucuk
+            // 
+            this.Txt_kucuk.Location = new System.Drawing.Point(357, 236);
+            this.Txt_kucuk.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Txt_kucuk.Name = "Txt_kucuk";
+            this.Txt_kucuk.Size = new System.Drawing.Size(121, 29);
+            this.Txt_kucuk.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(254, 292);
+            this.label7.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(91, 24);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "En Büyük";
+            // 
+            // Txt_buyuk
+            // 
+            this.Txt_buyuk.Location = new System.Drawing.Point(357, 292);
+            this.Txt_buyuk.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Txt_buyuk.Name = "Txt_buyuk";
+            this.Txt_buyuk.Size = new System.Drawing.Size(121, 29);
+            this.Txt_buyuk.TabIndex = 1;
+            // 
+            // Btn_Bul
+            // 
+            this.Btn_Bul.BackColor = System.Drawing.Color.LimeGreen;
+            this.Btn_Bul.ForeColor = System.Drawing.Color.Transparent;
+            this.Btn_Bul.Location = new System.Drawing.Point(255, 166);
+            this.Btn_Bul.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Btn_Bul.Name = "Btn_Bul";
+            this.Btn_Bul.Size = new System.Drawing.Size(231, 43);
+            this.Btn_Bul.TabIndex = 2;
+            this.Btn_Bul.Text = "Bul";
+            this.Btn_Bul.UseVisualStyleBackColor = false;
+            this.Btn_Bul.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label8.Location = new System.Drawing.Point(193, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(429, 24);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "En büyük en küçük sayiyiyi bulan form uygulamasi";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LimeGreen;
+            this.ClientSize = new System.Drawing.Size(804, 396);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.Btn_Bul);
+            this.Controls.Add(this.Txt_buyuk);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.Txt_kucuk);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.TxtSayi4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TxtSayi5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtSayi3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtSayi2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtSayı1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtSayı1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtSayi2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtSayi3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtSayi5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TxtSayi4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox Txt_kucuk;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox Txt_buyuk;
+        private System.Windows.Forms.Button Btn_Bul;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ GirilenBesSayidanBuyukKucukBulma/GirilenOnSayidanBuyukKucukBulma/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GirilenOnSayidanBuyukKucukBulma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int sayi1, sayi2, sayi3,sayi4,sayi5;
+            int enBuyuk, enKucuk;
+            sayi1 = Convert.ToInt32(TxtSayı1.Text);
+            sayi2 = Convert.ToInt32(TxtSayi2.Text);
+            sayi3 = Convert.ToInt32(TxtSayi3.Text);
+            sayi4= Convert.ToInt32(TxtSayi4.Text);
+            sayi5 = Convert.ToInt32(TxtSayi5.Text);
+
+            if(sayi1>sayi2 && sayi1>sayi3 && sayi1>sayi4 && sayi1>sayi5)
+            {
+                enBuyuk = sayi1;
+            }
+            else if (sayi2>sayi1 && sayi2>sayi3 && sayi2>sayi4 && sayi2>sayi5)  
+            {
+                enBuyuk= sayi2;
+            }
+            else if (sayi3>sayi1 && sayi3>sayi2 && sayi3>sayi4 && sayi3>sayi5)
+            {
+                enBuyuk = sayi3;
+            }
+            else if (sayi4>sayi1 && sayi4>sayi2 && sayi4 > sayi3 && sayi4 > sayi5)
+            {
+                enBuyuk = sayi4;
+            }
+            else
+            {
+                enBuyuk = sayi5;
+            }
+
+            Txt_buyuk.Text = enBuyuk.ToString();
+
+            if (sayi1 < sayi2 && sayi1 < sayi3 && sayi1 < sayi4 && sayi1 < sayi5)
+            {
+                enKucuk = sayi1;
+            }
+            else if (sayi2 < sayi1 && sayi2 < sayi3 && sayi2 < sayi4 && sayi2 < sayi5)
+            {
+                enKucuk = sayi2;
+            }
+            else if (sayi3 < sayi1 && sayi3 < sayi2 && sayi3 < sayi4 && sayi3 < sayi5)
+            {
+                enKucuk = sayi3;
+            }
+            else if (sayi4 < sayi1 && sayi4 < sayi2 && sayi4 < sayi3 && sayi4 < sayi5)
+            {
+                enKucuk = sayi4;
+            }
+            else 
+            {
+                enKucuk = sayi5;
+            }
+            Txt_kucuk.Text = enKucuk.ToString();
+          
+
+
+        }
+
+        int sayac = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            sayac++;
+            if(sayac %2 == 0)
+            {
+                label8.BackColor = Color.Aqua;
+            }
+            else
+            {
+                label8.BackColor = Color.Yellow;
+            }
+        }
+    }
+}
